73bdfe853267662f976e322bc23c4dffe4207236
Hattori Keigo <keigoht@gmail.com>
2019-01-18 15:39:07 +0900

    Move test

--- test/__init__.py
+++ test/__init__.py
@@ -3,12 +3,12 @@ import os
 from drucker.logger import JsonServiceLogger, JsonSystemLogger
 import drucker.drucker_worker_servicer
 
-from drucker_client.test.dummy_app import DummyApp
+from test.dummy_app import DummyApp
 from drucker_client.logger import logger
 
 
 os.environ["DRUCKER_TEST_MODE"] = "True"
-os.environ["DRUCKER_SETTINGS_YAML"] = "drucker_client/test/test-settings.yml"
+os.environ["DRUCKER_SETTINGS_YAML"] = "test/test-settings.yml"
 
 app = DummyApp()
 service_logger = JsonServiceLogger(app.config)
